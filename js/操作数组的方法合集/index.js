4bc040af1b3244a1305419244417e722abe6a8bf
tzl <tzl@weiyian.com>
2019-06-21 11:29:41 +0800

    update： includes  +  数据类型；

--- js/操作数组的方法合集/index.js
+++ js/操作数组的方法合集/index.js
@@ -3,7 +3,14 @@
 // 文章地址 ： https://mp.weixin.qq.com/s/z7P1rJ16VvzRweRnnWtbmg
 
 
-
-
-
-
+/**
+ * includes
+ * 用途： 检测数组中是否含有某元素；
+ * 例子： [1, 2, 3].includes(2);     // true
+ *
+ * arr.includes(searchElement, fromIndex)；
+ * 	searchElement	必须。需要查找的元素值。
+ *  fromIndex	可选。从该索引处开始查找 searchElement。如果为负值，则按升序从 array.length + fromIndex 的索引开始搜索。默认为 0。
+ * 如果fromIndex 大于等于数组长度 ，则返回 false 。该数组不会被搜索
+ * 如果 fromIndex 为负值，计算出的索引将作为开始搜索searchElement的位置。如果计算出的索引小于 0，则整个数组都会被搜索。
+ *  */
---
+++ js/数据类型相关/index.js
@@ -0,0 +1,28 @@
+/**
+ * js中共有6种数据类型：
+ * 包括5种基础数据类型 ： undefined,null,boolean,number,string;
+ * 1种复杂数据类型： object;
+ */
+
+
+
+/**
+ * typeof 操作符
+ * 用途： 测定变量的数据类型；
+ * 可返回6种数据类型：
+ * 		1.Undefined: 该值未定义；
+ * 		2.Boolean ：布尔型；
+ * 		3.Number： 数值型；
+ * 		4.String: 字符串；
+ * 		5.Function ： 函数
+ * 		6.Object: 对象（包括数组）或 null；
+ *
+ *  */
+
+/**
+ * Undefined 数据类型：
+ * 	声明变量但未赋值时 即为 Undefined
+ * 实例： var message;
+ * 		console.log(message == undefined )  //true;
+  *
+ */
